e6b01a8507a406653f5783bfe6ed3d731ff7b520
Aksenenko Oleg <aksenenko_oyu@drom.ru>
2022-05-07 13:04:24 +1000

    Refactoring

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,7 @@
+import enum
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Enum
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
@@ -18,20 +20,17 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-class IncomingVisitor(db.Model):
+class VisitorType(enum.Enum):
+    incoming = 1
+    leaving = 2
 
-    __tablename__ = "incoming_visitor"
+class Visitor(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, default=db.func.now())
-
-
-class LeavingVisitor(db.Model):
-
-    __tablename__ = "leaving_visitor"
+    __tablname__ = "visitor"
 
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.DateTime, default=db.func.now())
+    visitor_type = db.Column(Enum(VisitorType))
 
 
 class Admins(db.Model):
@@ -69,97 +68,100 @@ password=password).first()
 @app.route('/IncomingVisitor')
 def incoming_visitor():
     if str(request.headers.get('Magic-Number')) in trusted_devices:
-        income_visitor = IncomingVisitor(time=db.func.current_timestamp())
+        income_visitor = Visitor(
+            time=db.func.current_timestamp(),
+            visitor_type = VisitorType.incoming
+        )
         db.session.add(income_visitor)
         db.session.commit()
-        return f"Successfull add new visitor! 
-{IncomingVisitor.query.count()}"
+        return f"Successfull add new visitor! {Visitor.query.count()}"
     else:
         return "Bad device!"
 
 @app.route('/LeavingVisitor')
 def leaving_visitor():
     if str(request.headers.get('Magic-Number')) in trusted_devices:
-        leaving_visit = LeavingVisitor(time=db.func.current_timestamp())
-        db.session.add(leaving_visit)
+        leaving_visitor = Visitor(
+            time=db.func.current_timestamp(),
+            visitor_type = VisitorType.leaving)
+        db.session.add(leaving_visitor)
         db.session.commit()
         return f"Successfull add new leaving visitor! 
-{LeavingVisitor.query.count()}"
+{Visitor.query.count()}"
     else:
         return "Bad device!"
 
-@app.route('/IncomingVisitorLastMounth')
-def ivlm():
-    #result = db.engine.execute("SELECT * FROM incoming_visitor WHERE time 
-BETWEEN NOW() - INTERVAL 30 DAY AND NOW()")
+def get_day_income_people(visitor_type: VisitorType):
     now = datetime.now()
-    two_hours_ago = now - timedelta(hours=2)
+    day_ago = now - timedelta(days=1)
+    return Visitor.query.filter(
+        (Visitor.time > day_ago) &
+        (Visitor.visitor_type == visitor_type)
+    ).count()
 
-    # return all users created less then 2 hours ago
-    result = IncomingVisitor.query.filter(IncomingVisitor.time > 
-two_hours_ago).all()
-    #ids = [row[0] for row in result]
-    return str(result[0].time.minute)
+
+def get_mean_people_per_timedelta(timedelta_: int):
+
+    mean_people_per_timedelta = 0
+    incoming_people_per_timedelta = []
+    leaving_people_per_timedelta = []
+    first_day = datetime.now()
+    second_day = first_day - timedelta(days=1)
+
+    for i in range(timedelta_):
+        mean_people_per_timedelta += Visitor.query.filter(
+            (Visitor.time > second_day) &
+            (Visitor.time < first_day) &
+            (Visitor.visitor_type == VisitorType.incoming)
+        ).count()
+        incoming_people_per_timedelta.append(Visitor.query.filter(
+            (Visitor.time > second_day) &
+            (Visitor.time < first_day) &
+            (Visitor.visitor_type == VisitorType.incoming)
+        ).count())
+        leaving_people_per_timedelta.append(Visitor.query.filter(
+            (Visitor.time > second_day) &
+            (Visitor.time < first_day) &
+            (Visitor.visitor_type == VisitorType.leaving)
+        ).count())
+        first_day -= timedelta(days=1)
+        second_day -= timedelta(days=1)
+
+    mean_people_per_timedelta /= timedelta_
+
+    return mean_people_per_timedelta, incoming_people_per_timedelta, 
+leaving_people_per_timedelta
+
 
 @app.route('/PeoplePerDay')
 def people_per_day():
     global current_delay
-    now = datetime.now()
-    day_ago = now - timedelta(days=1)
 
-    day_income_people = IncomingVisitor.query.filter(IncomingVisitor.time 
-> day_ago).count()
-    day_leave_people = LeavingVisitor.query.filter(LeavingVisitor.time > 
-day_ago).count()
+    day_income_people = get_day_income_people(VisitorType.incoming)
+    day_leave_people = get_day_income_people(VisitorType.leaving)
 
-    mean_people_per_week = 0
-    incoming_people_per_week = []
-    leaving_people_per_week = []
-    first_day = datetime.now()
-    second_day = now - timedelta(days=1)
+    (
+        mean_people_per_week,
+        incoming_people_per_week,
+        leaving_people_per_week
+    ) = get_mean_people_per_timedelta(7)
 
-    for i in range(7):
-        mean_people_per_week += 
-IncomingVisitor.query.filter((IncomingVisitor.time > second_day) & 
-(IncomingVisitor.time < first_day)).count()
-        
-incoming_people_per_week.append(IncomingVisitor.query.filter((IncomingVisitor.time 
-> second_day) & (IncomingVisitor.time < first_day)).count())
-        
-leaving_people_per_week.append(LeavingVisitor.query.filter((LeavingVisitor.time 
-> second_day) & (LeavingVisitor.time < first_day)).count())
-        first_day -= timedelta(days=1)
-        second_day -= timedelta(days=1)
-
-    mean_people_per_week /= 7
-
-
-    mean_people_per_month = 0
-    day_incoming = []
-    day_leaving = []
-    first_day = datetime.now()
-    second_day = now - timedelta(days=1)
-
-    for i in range(30):
-        people_per_prev_day = 
-IncomingVisitor.query.filter((IncomingVisitor.time > second_day) & 
-(IncomingVisitor.time < first_day)).count()
-        mean_people_per_month += people_per_prev_day
-        day_incoming.append(people_per_prev_day)
-        
-day_leaving.append(LeavingVisitor.query.filter((LeavingVisitor.time > 
-second_day) & (LeavingVisitor.time < first_day)).count())
-        first_day -= timedelta(days=1)
-        second_day -= timedelta(days=1)
-
-    mean_people_per_month /= 30
+    (
+        mean_people_per_month,
+        day_incoming,
+        day_leaving
+    ) = get_mean_people_per_timedelta(30)
 
     incoming_visitors = dict()
-    for visitor in IncomingVisitor.query.all():
+    for visitor in Visitor.query.filter(
+        Visitor.visitor_type == VisitorType.incoming
+        ).all():
         incoming_visitors[visitor.id] = time_to_str(visitor.time)
 
     leaving_visitors = dict()
-    for visitor in LeavingVisitor.query.all():
+    for visitor in Visitor.query.filter(
+        Visitor.visitor_type == VisitorType.leaving
+        ).all():
         leaving_visitors[visitor.id] = time_to_str(visitor.time)
 
     return render_template("moderate.html",
@@ -178,14 +180,20 @@ second_day) & (LeavingVisitor.time < first_day)).count())
 @app.route('/DeleteIncomingVisitor', methods=["POST"])
 def delete_incoming_visitor():
     id = int(request.args.get('id'))
-    IncomingVisitor.query.filter(IncomingVisitor.id == id).delete()
+    Visitor.query.filter(
+        (Visitor.id == id) &
+        (Visitor.visitor_type == VisitorType.incoming)
+    ).delete()
     db.session.commit()
     return redirect(url_for('people_per_day'))
 
 @app.route('/DeleteLeavingVisitor', methods=["POST"])
 def delete_leaving_visitor():
     id = int(request.args.get('id'))
-    LeavingVisitor.query.filter(LeavingVisitor.id == id).delete()
+    Visitor.query.filter(
+        (Visitor.id == id) &
+        (Visitor.visitor_type == VisitorType.leaving)
+    ).delete()
     db.session.commit()
     return redirect(url_for('people_per_day'))
 
@@ -202,4 +210,3 @@ def get_delay():
 @app.route('/Test')
 def test():
     return str(request.headers.get('Magic-Number'))
-
